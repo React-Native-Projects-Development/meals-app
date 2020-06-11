b9daefffc760e0d22b9e06b7eeb64a041856cd76
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-11 00:31:23 -0600

    Adding enableScreens for better native performance

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
-
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
+import { enableScreens } from "react-native-screens";
+
+import MealsNavigator from "./navigation/MealsNavigator";
+
+enableScreens();
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -23,9 +26,5 @@ export default function App() {
     );
   }
 
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <MealsNavigator />;
 }
