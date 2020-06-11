5594e0ced9246258827d3eff338c0339fc5f6c58
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-11 00:36:21 -0600

    Refactoring UI and passing params from route

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 const CategoriesScreen = (props) => {
+  // When Using CategoriesScreen on our react navigation component these components get a special prop
+  // console.log(props);
+
+  const renderGridItem = (itemData) => {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: "CategoryMeals",
+            params: {
+              categoryId: itemData.item.id,
+            },
+          });
+        }}
+      />
+    );
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-    </View>
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
 };
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+// In JS Functions are objects therefore we can add properties to them
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+};
 
 export default CategoriesScreen;
