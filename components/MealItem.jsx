9e2827469ceaf1caa44c858d4aa711a4a67bd795
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-12 20:37:01 -0600

    Adding MealItem component

---
+++ components/MealItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MealItem = (props) => {
+    return (  );
+}
+
+const styles = StyleSheet.create({
+
+});
+ 
+export default MealItem;
