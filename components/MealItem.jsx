a197a1c0d80ecd08e8014ae32d19007fa2808bf3
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-13 00:05:30 -0600

    Receiving prop and adding UI to MealItem

--- components/MealItem.jsx
+++ components/MealItem.jsx
@@ -1,12 +1,80 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+  ImageBackground,
+} from "react-native";
 
 const MealItem = (props) => {
-    return (  );
-}
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+  return (
+    <View style={styles.mealItem}>
+      <TouchableCmp onPress={props.onSelectMeal} activeOpacity={0.6}>
+        <View>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+            <ImageBackground
+              source={{ uri: props.image }}
+              style={styles.bgImage}
+            >
+              <View style={styles.titleContainer}>
+                <Text style={styles.title}>{props.title}</Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+            <Text>{props.duration}m</Text>
+            <Text>{props.complexity.toUpperCase()}</Text>
+            <Text>{props.affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </TouchableCmp>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
+  mealItem: {
+    height: 200,
+    width: "100%",
+    backgroundColor: "#f5f5f5",
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
+  },
+  mealRow: {
+    flexDirection: "row",
+  },
+  mealHeader: {
+    height: "85%",
+  },
+  mealDetail: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "15%",
+  },
+  titleContainer: {
+    backgroundColor: "rgba(0,0,0,0.5)",
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 20,
+    color: "#fff",
+    textAlign: "center",
+  },
 });
- 
+
 export default MealItem;
