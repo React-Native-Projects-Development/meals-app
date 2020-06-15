2a3abfde072d7b7c1b136810ffdcf82609a3d196
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-15 17:40:16 -0600

    Displaying filtered meals and displaying fallback message when there's none present

--- screens/FavoritesScreen.jsx
+++ screens/FavoritesScreen.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import MealList from "../components/MealList";
-import { MEALS } from "../data/dummy-data";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
+import { useSelector } from "react-redux";
+import { View, Text, StyleSheet } from "react-native";
 
 const FavoritesScreen = (props) => {
-  const favMeals = MEALS.filter((meal) => meal.id === "m1" || meal.id === "m2");
+  const favMeals = useSelector((state) => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <Text>No favorites meals. Start adding some!</Text>
+      </View>
+    );
+  }
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
@@ -15,6 +24,7 @@ FavoritesScreen.navigationOptions = (navData) => {
     headerLeft: () => (
       <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
         <Item
+          title="Menu"
           iconName="ios-menu"
           onPress={() => {
             navData.navigation.toggleDrawer();
@@ -24,5 +34,13 @@ FavoritesScreen.navigationOptions = (navData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default FavoritesScreen;
