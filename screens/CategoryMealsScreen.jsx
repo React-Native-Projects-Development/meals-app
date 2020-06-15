bfc89870c3d7df15915264f7d320137b545e9ec7
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-15 17:39:22 -0600

    Removing meals and retrieving meals from global state

--- screens/CategoryMealsScreen.jsx
+++ screens/CategoryMealsScreen.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 
-import { CATEGORIES, MEALS } from "../data/dummy-data";
+import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
+import { useSelector } from "react-redux";
+import { View, StyleSheet } from "react-native";
+import DefaultText from "../components/DefaultText";
 
 const CategoryMealScreen = (props) => {
   const catId = props.navigation.getParam("categoryId");
 
-  const displayedMeals = MEALS.filter((meal) =>
+  const availableMeals = useSelector((state) => state.meals.filteredMeals);
+
+  const displayedMeals = availableMeals.filter((meal) =>
     meal.categoryIds.includes(catId)
   ); // -1 will be returned if the category is not part of the categoryIds if its 0 or greater than we know the catId of the meal is included in the categoryIds
+
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found. Please check your filters.</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
@@ -21,5 +34,13 @@ CategoryMealScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title,
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default CategoryMealScreen;
