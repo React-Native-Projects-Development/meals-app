26bbd1ef24767cfae8397aa14b5283cb82297e1c
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-15 16:56:48 -0600

    Added meals actions constants, action and reducer

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,4 +1,5 @@
 import { MEALS } from "../../data/dummy-data";
+import { TOGGLE_FAVORITE, SET_FILTERS } from "../actions";
 
 const initialMealsState = {
   meals: MEALS,
@@ -6,8 +7,51 @@ const initialMealsState = {
   favoriteMeals: [],
 };
 
-const meals = (state = initialMealsState, action) => {
-  return state;
-};
+export const mealsReducer = (state = initialMealsState, action) => {
+  switch (action.type) {
+    case TOGGLE_FAVORITE:
+      const existingIndex = state.favoriteMeals.findIndex(
+        (meal) => meal.id === action.mealId
+      );
+      if (existingIndex >= 0) {
+        const updatedFavMeals = [...state.favoriteMeals]; // here we create a new array and populate it with existing fav meals. this is a copy of the original array so we don't manipulate the original one.
+        updatedFavMeals.splice(existingIndex, 1);
+        return {
+          ...state,
+          favoriteMeals: updatedFavMeals,
+        };
+      } else {
+        const meal = state.meals.find((meal) => meal.id === action.mealId);
+        return {
+          ...state,
+          favoriteMeals: state.favoriteMeals.concat(meal),
+        };
+      }
+    case SET_FILTERS:
+      const appliedFilters = action.filters;
+      const updatedFilteredMeals = state.meals.filter((meal) => {
+        if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+          return false;
+        }
+        if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+          return false;
+        }
+        if (appliedFilters.vegetarian && !meal.isVegetarian) {
+          return false;
+        }
+        if (appliedFilters.vegan && !meal.isVegan) {
+          return false;
+        }
 
-export default mealsReducer;
+        return true;
+      });
+
+      return {
+        ...state,
+        filteredMeals: updatedFilteredMeals,
+      };
+
+    default:
+      return state;
+  }
+};
